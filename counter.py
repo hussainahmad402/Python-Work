45102387fb7ffd556d3e75e5532cf2d851398324
Hussain <hussainahmad9402@gmail.com>
2025-01-02 23:44:59 +0500

    Add the counter project

--- counter.py
+++ counter.py
@@ -9,7 +9,7 @@ root.geometry("400x400")
 # root.resizable(False, False)
 
 # Create a label to display the random number
-label = tk.Label(root, text="Click to generate a random number", font=("Agdasima", 14), bg="black", fg="white")
+label = tk.Label(root, text="Click to Count", font=("Agdasima", 14), bg="black", fg="white")
 label.pack(pady=20)
 
 label=tk.Label(root,text="",font=("Agdasima",14), bg="black",fg="White")
@@ -26,7 +26,7 @@ def on_button_click():
     label.config(text=f"Count: {number}",font=("Agdasima",14))
 
 # Create a button that calls the on_button_click function
-button = tk.Button(root, text="Generate Random Number", command=on_button_click,bg="#3D3D3D",fg="white",activebackground="#3D3D3D",activeforeground="white")
+button = tk.Button(root, text="Click To Count", command=on_button_click,bg="#3D3D3D",fg="white",activebackground="#3D3D3D",activeforeground="white")
 button.pack(pady=10)
 
 # Reset Button
